3d02dd00968f382f33fed927407db68ccc1b50d8
kamilbernaerts <kamil.bernaerts@gmail.com>
2026-03-24 22:04:16 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://kamilbernaerts.com',
+  site: 'https://kamilbernaerts.github.io',
+  base: '/website',
   build: {
     assets: '_assets'
   }
